cfb6d141aecd7f91ffd3f566c932cffc34469179
almynic1 <almynic1@students.zhaw.ch>
2017-03-16 20:54:25 +0100

    intialize seriesManager repo

---
+++ SeriesManager/seriesManager.nim
@@ -0,0 +1,132 @@
+import os
+import re
+import strutils
+
+var dataToBeProecessed : seq[string]   # sequence which contains the data which should be processed
+dataToBeProecessed = @[] # intialsize sequence dataToBeProcessed.
+let fileExtensions = [".avi",".mkv",".srt",".mp4"]
+let videoEncodingFormat = ["x264", "HDTV", "720p", "480p", "1080p", "Bluray", "YIFY"]
+var seriesNameFolders = [""]
+
+let episodePattern1 = re"[a-zA-Z][0-9][0-9][a-zA-Z][0-9][0-9]" # episode format s01e01
+let episodePattern2 = re"[a-zA-Z]+[0-9][0-9][a-zA-Z]+[0-9]+" #epsiode format season 1 epsiode 1
+let episodePattern3 = re"\d\d\d" #episode format 604
+let episodePattern4 = re"\d\dx\d\d" #episode format 60x13
+
+let yearPattern = re"\d\d\d\d" #year format {2012 , 1993 and so on}
+
+# Declaring function so that order of implementation doesn't matter
+proc processDirectory(workingDirectory: string)
+proc getSeriesNameFromFile(fileName: string): string
+proc move_data(fileOrDirectory : string, destDir : string)
+proc movingFoldersToSeriesFolders(folderName : string, destFolder : string)
+proc movingFilesToSeriesFolders(fileName : string, destFolder : string)
+proc stripFileEnding(fileName: string): string
+proc removeVideoEncodingInformationFromFileName(fileName: string): string
+proc isSeriesEpisode(fileName: string): bool
+proc isMovie(fileName: string): bool
+proc similarityCheckOnVariableLength()
+proc levensthein(fileName: string, serieFolder: string): int
+proc stripEverythingfromFileName(fileName: string): string
+
+#[ 
+    walks the entire working directory and adds all 
+    files to the dataToBeProcessed array
+    @param workingDirectory, the directory in which the seriesManager should do a cleanup
+    @return nothing
+]#
+
+proc removeDots(fileName: string): string = 
+    var fileNameWithoutDots = strutils.replace(fileName, ".", " ")
+    return fileNameWithoutDots 
+
+proc processDirectory(workingDirectory: string) =
+    for kind, path in walkDir(workingDirectory):
+        dataToBeProecessed.add(splitPath(path).tail)
+    for data in dataToBeProecessed:
+        var series_name = getSeriesNameFromFile(data)
+#[
+    moves the diretory to the destDir
+    @param fileOrDirectory, the file or directory to move
+    @param destDir, the destination directory in which the data should be moved
+]#
+proc move_data(fileOrDirectory : string, destDir : string) =
+    moveFile(fileOrDirectory, destDir)
+
+#[
+    extracts the series name from a file and returns the series name
+    @param fileName, the file name from which the series name should be extracted
+    @
+]#
+proc getSeriesNameFromFile(fileName: string): string =
+    var fileNameStripEverything = stripEverythingfromFileName(fileName)
+    return fileNameStripEverything
+
+#[
+    strips the file ending of the file name and returns the file name without any extension
+]#
+proc stripFileEnding(fileName : string) : string =
+    var (dir, name, ext) = splitFile(fileName)
+    var stripFileEnding = replace(fileName, $ext, "")
+    return stripFileEnding
+
+proc deleteEverythingAfterEpisodePattern(fileName: string): string = 
+    var fileName : string = fileName
+    if(fileName.contains(episodePattern1)):
+        var splitted : seq[string] = fileName.split(episodePattern1)
+        fileName = splitted[0]
+        echo fileName
+    return fileName
+
+proc movingFoldersToSeriesFolders(folderName : string, destFolder : string) =
+    echo ""
+proc movingFilesToSeriesFolders(fileName : string, destFolder : string) =
+    echo ""
+proc removeYearSequence(fileName : string):string =
+    if(fileName.contains(yearPattern)):
+        var fileName =  replace(fileName, yearPattern, "")
+    return fileName
+    
+proc removeVideoEncodingInformationFromFileName(fileName : string): string =
+    var fileNameWithoutEncoding : string = fileName
+    for encoding in videoEncodingFormat:
+        if(fileName.contains(re(encoding))):
+            fileNameWithoutEncoding = replace(fileNameWithoutEncoding, re(encoding), "")
+    return fileNameWithoutEncoding
+
+proc isSeriesEpisode(fileName: string): bool =
+    if (fileName.contains(episodePattern1)):
+        #echo "match pattern 1: s01e01"
+        return true
+    elif (fileName.contains(episodePattern2)):
+        #echo "match pattern 2: season 1 epsiode 1"
+        return true
+    elif (fileName.contains(episodePattern3)):
+        #echo "match pattern 3: 604"
+        return true
+    elif (fileName.contains(episodePattern4)):
+        #echo "match pattern 4: 60x13"
+        return true
+    else:
+        return false
+
+proc stripEverythingfromFileName(fileName : string): string =
+    return removeDots(removeYearSequence(removeVideoEncodingInformationFromFileName(stripFileEnding(fileName))))
+
+proc isMovie(fileName: string): bool =
+    var movieTitle = stripEverythingfromFileName(fileName)
+    if (not isSeriesEpisode(movieTitle)):
+        return true
+    else: 
+        return false
+
+proc similarityCheckOnVariableLength() =
+    echo ""
+
+proc levensthein(fileName: string, serieFolder: string): int =
+    var levenshteinDistance = editDistance(fileName, serieFolder)
+    return levenshteinDistance
+
+proc doesSeriesFolderExist(workingDirectory:string ,fileName: string): bool = 
+    if(dataToBeProecessed.contains(getSeriesNameFromFile(fileName))):
+        return true
---
+++ SeriesManager/seriesManagerTest.nim
@@ -0,0 +1,48 @@
+import unittest
+include seriesManager
+
+suite "test for seriesManager":
+    echo "starting tests: "
+
+    teardown:
+        echo "finished test"
+
+    test "test removing fileending":
+            var stripFileEnding = stripFileEnding("chicago.justice.s01e04.HDTV.x265-SVA[Prime].mkv") 
+            checkpoint("chicago.justice.s01e04.HDTV.x265-SVA[Prime].mkv should be chicago.justice.s01e04.HDTV.x265-SVA[Prime]")
+            check:
+                stripFileEnding == "chicago.justice.s01e04.HDTV.x265-SVA[Prime]"
+            checkpoint("sstest.ssd.avi should be sstest.ssd")
+            check:
+                stripFileEnding("sstest.ssd.avi") == "sstest.ssd"
+    
+    test "test for detection of serie episodes and movies":
+        checkpoint("series episode chicago.justice.s01e04.HDTV.x265-SVA[Prime].mkv should be detected as serie episode")
+        check:
+            isSeriesEpisode("chicago.justice.s01e04.HDTV.x265-SVA[Prime].mkv") == true
+        check:
+            isMovie("The.Wolverine.2013.1080p.Bluray.x264.YIFY.mp4") == true  
+
+    
+    test "test get seriesName from file":
+        checkpoint("chicago.justice.s01e04.HDTV.x265-SVA[Prime].mkv should result in series name chicago justice")
+        check:
+            getSeriesNameFromFile("chicago.justice.s01e04.HDTV.x265-SVA[Prime].mkv") == "chicago justice"
+    
+    test "test episode pattern 1 06x21":
+        var pattern = re"\d\dx\d\d"
+        checkpoint("assert that pattern works")
+        check("The.Wolverine.2013.1080p.Bluray.x264.YIFY.mp4".contains(pattern) == false)
+    
+    test "levensthein distance":
+        checkpoint("test that string 'home' and 'goal' has distance 3")
+        check: 
+            levensthein("home", "goal") == 3
+        checkpoint("test that string 'chicago justice' und 'chicago' has distance 8")
+        check:
+            levensthein("chicago justice", "chicago") == 8
+        check:
+            levensthein("", "") == 0
+        check:
+            levensthein("", "") != 1
+echo "finished"
